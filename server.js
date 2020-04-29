b4ceab53c4edb0d7ffcbce2939ffdb1a4ffc7ef0
Liudmila02 <vasulivna.02@gmail.com>
2020-04-29 11:08:57 +0300

    cors

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ app.use(
     credentials: true,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", 'true'); 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
